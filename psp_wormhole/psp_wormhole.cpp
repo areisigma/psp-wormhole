da7c9a6a24e287a32515c7a6fd2352d302fc04f1
areisigma <80626984+areisigma@users.noreply.github.com>
2022-06-18 09:03:22 +0200

    Add files via upload

---
+++ psp_wormhole/psp_wormhole.cpp
@@ -0,0 +1,185 @@
+﻿/*
+	PSP Low Level WiFi Proxy
+
+	Author:
+		Olaf Struck
+
+	Date:
+		12.06.2022 (Birth bigger knowledge)
+
+
+	I tried many solutions, but this seems to be the best. Low Level always wins. Simplicity in complexity!
+
+*/
+
+#pragma comment(lib,  "ws2_32.lib")
+
+#include <iostream>
+#include <stdio.h>
+
+#include "pcap.h"
+
+// C:\Program Files (x86)\Windows Kits\10\Include\10.0.17763.0\um
+// Later I will include all headers to local project folder for everyone to have no issues to run.
+
+
+#include <winsock.h>
+//#include <WS2tcpip.h>
+#include <tchar.h>
+
+
+BOOL LoadNpcapDlls()
+{
+	TCHAR npcap_dir[512];
+	UINT len;
+	len = GetSystemDirectory(npcap_dir, 480);
+	if (!len) {
+		fprintf(stderr, "Error in GetSystemDirectory: %x", GetLastError());
+		return FALSE;
+	}
+	_tcscat_s(npcap_dir, 512, TEXT("\\Npcap"));
+	if (SetDllDirectory(npcap_dir) == 0) {
+		fprintf(stderr, "Error in SetDllDirectory: %x", GetLastError());
+		return FALSE;
+	}
+	return TRUE;
+}
+
+
+// Function prototypes
+void ifprint(pcap_if_t *d);
+char *iptos(u_long in);
+char* ip6tos(struct sockaddr *sockaddr, char *address, int addrlen);
+
+
+// Print available devices.
+// I need is some basic networking. Sending/Capturing Beacon/Probe frames.
+
+int main(int argc, char *argv[]) {
+
+	pcap_if_t *alldevs;
+	pcap_if_t *d;
+	char errbuf[PCAP_ERRBUF_SIZE + 1];
+	char source[PCAP_ERRBUF_SIZE + 1];
+
+	// Load Npcap and its functions. 
+	if (!LoadNpcapDlls())
+	{
+		fprintf(stderr, "Couldn't load Npcap\n");
+		exit(1);
+	}
+
+	printf("Enter the device you want to list:\n"
+		"rpcap://              ==> lists interfaces in the local machine\n"
+		"rpcap://hostname:port ==> lists interfaces in a remote machine\n"
+		"                          (rpcapd daemon must be up and running\n"
+		"                           and it must accept 'null' authentication)\n"
+		"file://foldername     ==> lists all pcap files in the give folder\n\n"
+		"Enter your choice: ");
+
+	fgets(source, PCAP_ERRBUF_SIZE, stdin);
+	source[PCAP_ERRBUF_SIZE] = '\0';
+
+	// Retrieve the interfaces list 
+	if (pcap_findalldevs_ex(source, NULL, &alldevs, errbuf) == -1)
+	{
+		fprintf(stderr, "Error in pcap_findalldevs: %s\n", errbuf);
+		exit(1);
+	}
+
+	// Scan the list printing every entry 
+	for (d = alldevs; d; d = d->next)
+	{
+		ifprint(d);
+	}
+
+	pcap_freealldevs(alldevs);
+
+	return 1;
+}
+
+
+
+
+
+// Print all the available information on the given interface 
+void ifprint(pcap_if_t *d)
+{
+	pcap_addr_t *a;
+	char ip6str[128];
+
+	// Name
+	printf("%s\n", d->name);
+
+	// Description 
+	if (d->description)
+		printf("\tDescription: %s\n", d->description);
+
+	// Loopback Address
+	printf("\tLoopback: %s\n", (d->flags & PCAP_IF_LOOPBACK) ? "yes" : "no");
+
+	// IP addresses 
+	for (a = d->addresses; a; a = a->next) {
+		printf("\tAddress Family: #%d\n", a->addr->sa_family);
+
+		switch (a->addr->sa_family)
+		{
+		case AF_INET:
+			printf("\tAddress Family Name: AF_INET\n");
+			if (a->addr)
+				printf("\tAddress: %s\n", iptos(((struct sockaddr_in *)a->addr)->sin_addr.s_addr));
+			if (a->netmask)
+				printf("\tNetmask: %s\n", iptos(((struct sockaddr_in *)a->netmask)->sin_addr.s_addr));
+			if (a->broadaddr)
+				printf("\tBroadcast Address: %s\n", iptos(((struct sockaddr_in *)a->broadaddr)->sin_addr.s_addr));
+			if (a->dstaddr)
+				printf("\tDestination Address: %s\n", iptos(((struct sockaddr_in *)a->dstaddr)->sin_addr.s_addr));
+			break;
+
+		case AF_INET6:
+			printf("\tAddress Family Name: AF_INET6\n");
+			if (a->addr)
+				printf("\tAddress: %s\n", ip6tos(a->addr, ip6str, sizeof(ip6str)));
+			break;
+
+		default:
+			printf("\tAddress Family Name: Unknown\n");
+			break;
+		}
+	}
+	printf("\n");
+}
+
+
+
+// From tcptraceroute, convert a numeric IP address to a string 
+#define IPTOSBUFFERS	12
+char *iptos(u_long in)
+{
+	static char output[IPTOSBUFFERS][3 * 4 + 3 + 1];
+	static short which;
+	u_char *p;
+
+	p = (u_char *)&in;
+	which = (which + 1 == IPTOSBUFFERS ? 0 : which + 1);
+	_snprintf_s(output[which], sizeof(output[which]), sizeof(output[which]), "%d.%d.%d.%d", p[0], p[1], p[2], p[3]);
+	return output[which];
+}
+
+char* ip6tos(struct sockaddr *sockaddr, char *address, int addrlen)
+{
+	socklen_t sockaddrlen;
+
+
+	sockaddrlen = sizeof(struct sockaddr_in6);
+
+	if (getnameinfo(sockaddr,
+		sockaddrlen,
+		address,
+		addrlen,
+		NULL,
+		0,
+		NI_NUMERICHOST) != 0) address = NULL;
+
+	return address;
+}
